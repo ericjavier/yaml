e81e05f8066052f4f4807e45237278a76fafad37
Eric Javier Hernandez Saura <ericjavierhernandezsaura@gmail.com>
2015-01-12 10:49:12 -0800

    last implemented and tested

---
+++ include/yaml/sequence/last.hpp
@@ -0,0 +1,49 @@
+#ifndef LAST_HPP_INCLUDED
+#define LAST_HPP_INCLUDED
+
+#include <yaml\config.hpp>
+#include <yaml\core.hpp>
+#include <yaml\sequence\sequence_def.hpp>
+#include <yaml\sequence\is_empty.hpp>
+#include <yaml\sequence\last.hpp>
+
+#include <type_traits>
+
+BEGIN_YAML_NSP
+
+BEGIN_DETAIL_NSP
+
+template<typename S> class last_tmpl {
+
+  template<typename> struct impl;
+
+  template<typename H, typename R> struct impl<seq<H, R>> {
+
+    template<typename S, typename L> struct helper;
+
+    template<typename H, typename R> struct helper<seq<H, R>, std::false_type> {
+      using type = typename impl<force_t<R>>::type;
+    };
+
+    template<typename H, typename R> struct helper<seq<H, R>, std::true_type> {
+      using type = H;
+    };
+
+    using is_last = typename apply<is_empty, R>::type;
+    using type = typename helper<seq<H, R>, is_last>::type;
+
+  };
+
+public:
+
+  using type = typename impl<force_t<S>>::type;
+
+};
+
+END_DETAIL_NSP
+
+using last = curried_func_t<DETAIL_NSP_REF last_tmpl>;
+
+END_YAML_NSP
+
+#endif LAST_HPP_INCLUDED
---
+++ test/last.cpp
@@ -0,0 +1,19 @@
+#include <gtest\gtest.h>
+#include <yaml\config.hpp>
+#include <yaml\detail\cond.hpp>
+#include <yaml\sequence\last.hpp>
+#include "test_utils.hpp"
+
+using namespace std;
+using namespace YAML_NSP;
+using namespace DETAIL_NSP;
+
+TEST(last, seq) {
+  expect_yaml_is_same<t<4>, last_tmpl<seq1>::type>();
+  expect_yaml_is_same<t<1>, last_tmpl<seq2>::type>();
+}
+
+TEST(last, list) {
+  expect_yaml_is_same<t<4>, last_tmpl<lst1>::type>();
+  expect_yaml_is_same<t<1>, last_tmpl<lst2>::type>();
+}
