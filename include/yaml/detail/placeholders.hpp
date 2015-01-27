cf8ad1936255460e90d2fe64b45361975f01fc6e
Eric Javier Hernandez Saura <ericjavierhernandezsaura@gmail.com>
2015-01-26 16:45:44 -0800

    more sugarized placeholders defined

--- include/yaml/detail/placeholders.hpp
+++ include/yaml/detail/placeholders.hpp
@@ -17,6 +17,14 @@ END_DETAIL_NSP
 
 using _0 = DETAIL_NSP_REF ph<0>;
 using _1 = DETAIL_NSP_REF ph<1>;
+using _2 = DETAIL_NSP_REF ph<2>;
+using _3 = DETAIL_NSP_REF ph<3>;
+using _4 = DETAIL_NSP_REF ph<4>;
+using _5 = DETAIL_NSP_REF ph<5>;
+using _6 = DETAIL_NSP_REF ph<6>;
+using _7 = DETAIL_NSP_REF ph<7>;
+using _8 = DETAIL_NSP_REF ph<8>;
+using _9 = DETAIL_NSP_REF ph<9>;
 
 END_YAML_NSP
 
