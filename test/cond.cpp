ffd63b897cdb88eb80dbb0ae9795c8039120de56
Eric Javier Hernandez Saura <ericjavierhernandezsaura@gmail.com>
2015-02-12 14:06:53 -0800

    uncommenting cond.cpp tests

--- test/cond.cpp
+++ test/cond.cpp
@@ -1,24 +1,24 @@
-//#include <gtest/gtest.h>
-//#include <yaml/config.hpp>
-//#include <yaml/detail/cond.hpp>
-//
-//#include "test_utils.hpp"
-//
-//using namespace YAML_NSP;
-//using namespace DETAIL_NSP;
-//
-//TEST(cond, then_case) {
-//  expect_std_is_same<
-//    cond<have_type_t<std::integral_constant<int, 0>>,
-//    std::integral_constant<int, 0>,
-//    std::integral_constant<int, 1>>::type,
-//    std::integral_constant<int, 0>>();
-//}
-//
-//TEST(cond, else_case) {
-//  expect_std_is_same<
-//    cond<have_type_t<std::plus<int>>,
-//    inconditional_force_type_of<std::plus<int>>,
-//    std::integral_constant<int, 0>>::type,
-//    std::integral_constant<int, 0>>();
-//}
+#include <gtest/gtest.h>
+#include <yaml/config.hpp>
+#include <yaml/detail/cond.hpp>
+
+#include "test_utils.hpp"
+
+using namespace YAML_NSP;
+using namespace DETAIL_NSP;
+
+TEST(cond, then_case) {
+  expect_std_is_same<
+    cond<have_type_t<std::integral_constant<int, 0>>,
+    std::integral_constant<int, 0>,
+    std::integral_constant<int, 1>>::type,
+    std::integral_constant<int, 0>>();
+}
+
+TEST(cond, else_case) {
+  expect_std_is_same<
+    cond<have_type_t<std::plus<int>>,
+    inconditional_force_type_of<std::plus<int>>,
+    std::integral_constant<int, 0>>::type,
+    std::integral_constant<int, 0>>();
+}
