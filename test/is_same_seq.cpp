daf31905e26db37755373f090e218efd96adcee0
Eric Javier Hernandez Saura <ericjavierhernandezsaura@gmail.com>
2015-02-14 08:02:05 -0800

    updating is_same_seq tests to use F::ret<...> syntax instead of details::F_tmpl<...>::type

--- test/is_same_seq.cpp
+++ test/is_same_seq.cpp
@@ -5,14 +5,13 @@
 #include "test_utils.hpp"
 
 using namespace YAML_NSP;
-using namespace DETAIL_NSP;
 
 TEST(is_same_seq, true) {
-  expect_std_true_type<is_same_seq_tmpl<seq1, lst1>::type>();
-  expect_std_true_type<is_same_seq_tmpl<seq2, lst2>::type>();
+  expect_std_true_type<is_same_seq::ret<seq1, lst1>>();
+  expect_std_true_type<is_same_seq::ret<seq2, lst2>>();
 }
 
 TEST(is_same_seq, false) {
-  expect_std_false_type<is_same_seq_tmpl<seq1, lst2>::type>();
-  expect_std_false_type<is_same_seq_tmpl<seq2, lst1>::type>();
+  expect_std_false_type<is_same_seq::ret<seq1, lst2>>();
+  expect_std_false_type<is_same_seq::ret<seq2, lst1>>();
 }
